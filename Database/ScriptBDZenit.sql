74f340098ecee7cf05fef4f1b8dbf90a054df2e1
arthurRFmoraes <arthur.moraes@sptech.school>
2023-04-18 17:57:13 -0300

    modificacoes feitas no banco de dados

--- Database/ScriptBDZenit.sql
+++ Database/ScriptBDZenit.sql
@@ -1,56 +1,72 @@
-create database  Zenit;
+create database  zenit;
 
-use Zenit;
-Database changed
-create table Empresa
-    (idEmpresa int primary key auto_increment ,
-    nomeEmpresa varchar(45) ,
-    CNPJ char(14) ,
-    contatoEmpresa varchar(45));
+use zenit;
+
+create table empresa(
+    idEmpresa int primary key auto_increment ,
+    nomeEmpresa varchar(45) not null,
+    CNPJ char(14) not null ,
+    contatoEmpresa varchar(45)
+	)
+;
 
 
-create table Endereço (
+create table endereco (
     idEndereco int primary key auto_increment ,
-    Rua varchar(45) ,
-    Bairro varchar(45) ,
+    rua varchar(45) ,
+    bairro varchar(45) ,
     cidade varchar(45) ,
-    CEP varchar(45),
+    cep varchar(45),
     numero varchar (45),
     fkEmpresa int ,
-    foreign key (fkEmpresa) references Empresa (idEmpresa));
+    foreign key (fkEmpresa) references empresa (idEmpresa)
+    )
+;
 
 
-create table Transformadores (
+create table transformadores (
     idTransformadores int primary key auto_increment ,
     tamanhoTransformadores varchar(45) ,
     taxaTransferencia varchar(45) ,
     fkEmpresa int ,
-    foreign key (fkEmpresa) references Empresa (idEmpresa));
+    foreign key (fkEmpresa) references Empresa (idEmpresa)
+    )
+;
 
 
-create table Sensores (
+create table sensores (
     idSensores int primary key auto_increment,
     tipoSensor varchar(45),
     fkTransformador int ,
-    foreign key (fkTransformador) references Transformadores (idTransformadores));
+    foreign key (fkTransformador) references transformadores (idTransformadores)
+    )
+;
 
 
-create table Dados (
+create table dados (
     idDados int primary key auto_increment ,
     dataHoraDados datetime ,
     valorDados Decimal(5,2) ,
     fkSensor int ,
-    foreign key (fkSensor) references Sensores (idSensores));
+    foreign key (fkSensor) references sensores (idSensores)
+    )
+;
 
-
-
-create table Usuario (
+create table usuario (
     idUsuario int primary key auto_increment ,
     nomeUsuario varchar(45) ,
     senhaUsuario varchar(45) ,
     emailUsuario varchar(45) ,
     fkEmpresa int ,
-    foreign key (fkEmpresa) references Empresa (idEmpresa));
+    foreign key (fkEmpresa) references empresa (idEmpresa)
+    )
+;
+desc empresa;
+desc endereco;
+desc transformadores;
+desc sensores;
+desc dados;
+desc usuario;
 
 
 
