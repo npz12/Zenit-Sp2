4f2988e8a7d1c37f1bfa5e7d260a64d945a85280
arthurRFmoraes <arthur.moraes@sptech.school>
2023-06-04 22:32:22 -0300

    botao de trocar transformador e cores de alerta funcionando

--- site/public/login/alerta.js
+++ site/public/login/alerta.js
@@ -68,10 +68,26 @@ function alertar(resposta, idTransformador) {
         // exibirAlerta(temp, idTransformador, grauDeAviso, grauDeAvisoCor)
     }
 
+
     var card;
 
     if (idTransformador == 1) {
         temp_transf_1.innerHTML = temp + "°C";
+        if (temp >= limites.muito_quente) {
+            temp_transf_1.style = 'color: red;'
+        }
+        else if (temp < limites.muito_quente && temp >= limites.quente) {
+            temp_transf_1.style = 'background-color: yellow;'
+        }
+        else if (temp < limites.quente && temp > limites.frio) {
+            temp_transf_1.style = 'color: green;'
+        }
+        else if (temp <= limites.frio && temp > limites.muito_frio) {
+            temp_transf_1.style = 'background-color: yellow;'
+        }
+        else if (temp <= limites.muito_frio) {
+            temp_transf_1.style = 'color: red;'
+        }
         card = card_1
     } else if (idTransformador == 2) {
         temp_transf_2.innerHTML = temp + "°C";
